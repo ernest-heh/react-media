f7a08069e28398ec6438b9e31d53872b18bc27ce
Ernest Ho <ernest.heh@gmail.com>
2023-05-02 17:55:12 +0800

    add loading screen before rendering UsersList with createAsyncThunk

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const response = await axios.get("http://localhost:3005/users");
+
+  // DELAY LOADING...
+  await pause(1000);
+
+  return response.data;
+});
+
+// DEVELOPMENT ONLY
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+//
+
+export { fetchUsers };
