611758d1df02b2f4fff1aed9ab472a5fd4198b7c
Ernest Ho <ernest.heh@gmail.com>
2023-05-04 17:04:31 +0800

    add function to fetch all albums, add an album

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+// DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+    // PAUSE WITH SPINNER DEV ONLY
+    fetchFn: async (...args) => {
+      await pause(1000);
+      return fetch(...args);
+    },
+  }),
+  endpoints(builder) {
+    return {
+      addAlbum: builder.mutation({
+        invalidatesTags: (result, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
+      fetchAlbums: builder.query({
+        providesTags: (result, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
+        query: (user) => {
+          // user is an object
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export { albumsApi };
